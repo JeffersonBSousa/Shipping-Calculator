c26c66f68fcde0cb4906da11b898a6ba2a05915e
Jaedson <jaedson33@gmail.com>
2024-09-04 20:37:36 -0300

    fix pdf generation

--- src/app/report.tsx
+++ src/app/report.tsx
@@ -1,55 +1,70 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Alert, ScrollView } from 'react-native';
-import { useRouter } from 'expo-router';
-import useFormStore from '../store/useFormStore';
-import { shareAsync } from 'expo-sharing';
-import * as Print from 'expo-print';
-import * as MediaLibrary from 'expo-media-library';
-import * as FileSystem from 'expo-file-system';
-import { Asset } from 'expo-asset';
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, Button, Alert, ScrollView } from 'react-native'
+import { useRouter } from 'expo-router'
+import useFormStore from '../store/useFormStore'
+import { shareAsync } from 'expo-sharing'
+import * as Print from 'expo-print'
+import * as MediaLibrary from 'expo-media-library'
+import * as FileSystem from 'expo-file-system'
+import { Asset } from 'expo-asset'
 
-const logo = require('../../assets/images/logo.png');
+const logo = require('../../assets/images/logo.png')
 
 async function convertImageToBase64(imageUri: string): Promise<string | null> {
-    try {
-        const base64 = await FileSystem.readAsStringAsync(imageUri, {
-            encoding: FileSystem.EncodingType.Base64,
-        });
-        return `data:image/png;base64,${base64}`;
-    } catch (error) {
-        console.error('Erro ao converter imagem para Base64:', error);
-        return null;
-    }
+  try {
+    const base64 = await FileSystem.readAsStringAsync(imageUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    })
+    return `data:image/png;base64,${base64}`
+  } catch (error) {
+    console.error('Erro ao converter imagem para Base64:', error)
+    return null
+  }
 }
 
 const Report = () => {
-    const { viagens, removeViagem, removeAllViagens, calcularTotais, mediaCaminhao, nomeMotorista, modeloCaminhao } = useFormStore();
-    const router = useRouter();
+  const {
+    viagens,
+    removeViagem,
+    removeAllViagens,
+    calcularTotais,
+    mediaCaminhao,
+    nomeMotorista,
+    modeloCaminhao,
+  } = useFormStore()
+  const router = useRouter()
 
-    useEffect(() => {
-        const requestPermissions = async () => {
-            const { status } = await MediaLibrary.requestPermissionsAsync();
-            if (status !== 'granted') {
-                Alert.alert('Permissão necessária', 'Você precisa conceder permissão para gerar o PDF.');
-            }
-        };
-        requestPermissions();
-    }, []);
+  useEffect(() => {
+    const requestPermissions = async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync()
+      if (status !== 'granted') {
+        Alert.alert(
+          'Permissão necessária',
+          'Você precisa conceder permissão para gerar o PDF.'
+        )
+      }
+    }
+    requestPermissions()
+  }, [])
 
-    const handleGeneratePdf = async () => {
-        const { totalFretes, totalLucro, totalDistancia, totalCustos } = calcularTotais();
+  const handleGeneratePdf = async () => {
+    const { totalFretes, totalLucro, totalDistancia, totalCustos } =
+      calcularTotais()
 
-        const imageAsset = Asset.fromModule(logo);
-        await imageAsset.downloadAsync(); 
+    const imageAsset = Asset.fromModule(logo)
+    await imageAsset.downloadAsync()
 
-        const base64Image = await convertImageToBase64(imageAsset.localUri || '');
+    const base64Image = await convertImageToBase64(imageAsset.localUri || '')
 
-        if (!base64Image) {
-            Alert.alert('Erro', 'Não foi possível carregar a imagem. Verifique o caminho da imagem.');
-            return;
-        }
+    if (!base64Image) {
+      Alert.alert(
+        'Erro',
+        'Não foi possível carregar a imagem. Verifique o caminho da imagem.'
+      )
+      return
+    }
 
-        const htmlContent = `
+    const htmlContent = `
         <html>
 <head>
     <style>
@@ -58,7 +73,7 @@ const Report = () => {
         }
         body {
             font-family: Arial, sans-serif;
-            margin: 20px;
+            margin: 0 20px;
         }
         .header {
             position: fixed;
@@ -82,14 +97,11 @@ const Report = () => {
         .header-image {
             height: 120px;
         }
-        .content {
-            margin-top: 200px;
-        }
         .grid-container {
             display: grid;
             grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
             gap: 20px;
-            margin-top: 100px; /* Reset margin-top for grid-container */
+            align-items: end;
         }
         .grid-item {
             border: 1px solid #ddd;
@@ -108,7 +120,7 @@ const Report = () => {
         }
         .page-break {
             page-break-before: always;
-            height: 0;
+            margin-top: 200px;
         }
         
     </style>
@@ -124,133 +136,157 @@ const Report = () => {
     </div>
     <div class="content">
         <div class="grid-container">
-            ${viagens.map((viagem, index) => `
-                <div class="grid-item">
+            ${viagens
+              .map(
+                (viagem, index) => `
+                <div class="grid-item ${index % 6 === 0 ? 'page-break' : ''}">
                     <h3>${viagem.nomeViagem}</h3>
                     <p><strong>Frete:</strong> R$ ${viagem.valorFrete}</p>
-                    <p><strong>Distância:</strong> ${viagem.distanciaViagem} Km</p>
-                    <p><strong>Custo Combustível:</strong> R$ ${(parseFloat(viagem.preçoCombustivel) * (parseFloat(viagem.distanciaViagem) / parseFloat(mediaCaminhao || '1'))).toFixed(2)}</p>
-                    <p><strong>Alimentação:</strong> R$ ${viagem.alimentação}</p>
-                    <p><strong>Pagamento Ajudante:</strong> R$ ${viagem.pagamentoAjudante}</p>
-                    <p><strong>Outros Custos:</strong> R$ ${viagem.outrosCustos}</p>
+                    <p><strong>Distância:</strong> ${
+                      viagem.distanciaViagem
+                    } Km</p>
+                    <p><strong>Custo Combustível:</strong> R$ ${(
+                      parseFloat(viagem.preçoCombustivel) *
+                      (parseFloat(viagem.distanciaViagem) /
+                        parseFloat(mediaCaminhao || '1'))
+                    ).toFixed(2)}</p>
+                    <p><strong>Alimentação:</strong> R$ ${
+                      viagem.alimentação
+                    }</p>
+                    <p><strong>Pagamento Ajudante:</strong> R$ ${
+                      viagem.pagamentoAjudante
+                    }</p>
+                    <p><strong>Outros Custos:</strong> R$ ${
+                      viagem.outrosCustos
+                    }</p>
                     <p><strong>Lucro:</strong> R$ ${viagem.lucro}</p>
                 </div>
-                ${index % 6 === 5 ? '<div class="page-break"></div>' : ''}
-            `).join('')}
+            `
+              )
+              .join('')}
         </div>
     </div>
 </body>
-</html>`;
+</html>`
 
+    try {
+      const { uri } = await Print.printToFileAsync({ html: htmlContent })
 
+      await shareAsync(uri, { mimeType: 'application/pdf' })
+    } catch (error) {
+      console.error('Erro ao gerar o PDF:', error)
+      Alert.alert(
+        'Erro',
+        'Não foi possível gerar o PDF. Por favor, tente novamente.'
+      )
+    }
+  }
 
+  const handleRemoveViagem = (index: number) => {
+    Alert.alert('Confirmar', 'Você realmente deseja excluir esta viagem?', [
+      { text: 'Cancelar', style: 'cancel' },
+      { text: 'Excluir', onPress: () => removeViagem(index) },
+    ])
+  }
 
-        try {
-            const { uri } = await Print.printToFileAsync({ html: htmlContent });
+  const handleRemoveAll = () => {
+    Alert.alert(
+      'Confirmar',
+      'Você realmente deseja excluir todas as viagens?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        { text: 'Excluir', onPress: removeAllViagens },
+      ]
+    )
+  }
 
-            await shareAsync(uri, { mimeType: 'application/pdf' });
-        } catch (error) {
-            console.error('Erro ao gerar o PDF:', error);
-            Alert.alert('Erro', 'Não foi possível gerar o PDF. Por favor, tente novamente.');
-        }
-    };
+  const handleEditViagem = (index: number) => {
+    const viagem = viagens[index]
+    router.push({
+      pathname: '/EditViagem',
+      params: {
+        viagem: JSON.stringify(viagem),
+        index: index.toString(),
+      },
+    })
+  }
 
-    const handleRemoveViagem = (index: number) => {
-        Alert.alert(
-            'Confirmar',
-            'Você realmente deseja excluir esta viagem?',
-            [
-                { text: 'Cancelar', style: 'cancel' },
-                { text: 'Excluir', onPress: () => removeViagem(index) },
-            ]
-        );
-    };
-
-    const handleRemoveAll = () => {
-        Alert.alert(
-            'Confirmar',
-            'Você realmente deseja excluir todas as viagens?',
-            [
-                { text: 'Cancelar', style: 'cancel' },
-                { text: 'Excluir', onPress: removeAllViagens },
-            ]
-        );
-    };
-
-    const handleEditViagem = (index: number) => {
-        const viagem = viagens[index];
-        router.push({
-            pathname: '/EditViagem',
-            params: {
-                viagem: JSON.stringify(viagem),
-                index: index.toString(),
-            },
-        });
-    };
-
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                <Text style={styles.title}>Relatório de Viagens</Text>
-                {viagens.length === 0 ? (
-                    <Text style={styles.message}>Nenhuma viagem registrada.</Text>
-                ) : (
-                    viagens.map((viagem, index) => (
-                        <View key={index} style={styles.viagemContainer}>
-                            <Text style={styles.viagemTitle}>{viagem.nomeViagem}</Text>
-                            <Text>Frete: R$ {viagem.valorFrete}</Text>
-                            <Text>Distância: {viagem.distanciaViagem} Km</Text>
-                            <Text>Custo Combustível: R$ {(parseFloat(viagem.preçoCombustivel) * (parseFloat(viagem.distanciaViagem) / parseFloat(mediaCaminhao || '1'))).toFixed(2)}</Text>
-                            <Text>Alimentação: R$ {viagem.alimentação}</Text>
-                            <Text>Pagamento Ajudante: R$ {viagem.pagamentoAjudante}</Text>
-                            <Text>Outros Custos: R$ {viagem.outrosCustos}</Text>
-                            <Text>Lucro: R$ {viagem.lucro}</Text>
-                            <View style={styles.aa}>
-                                <Button title="Editar" onPress={() => handleEditViagem(index)} />
-                                <Button title="Excluir" onPress={() => handleRemoveViagem(index)} />
-                            </View>
-                        </View>
-                    ))
-                )}
-                <View style={styles.aa}>
-                    <Button title="Excluir Todas as Viagens" onPress={handleRemoveAll} />
-                    <Button title="Gerar PDF" onPress={handleGeneratePdf} />
-                </View>
-            </ScrollView>
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>Relatório de Viagens</Text>
+        {viagens.length === 0 ? (
+          <Text style={styles.message}>Nenhuma viagem registrada.</Text>
+        ) : (
+          viagens.map((viagem, index) => (
+            <View key={index} style={styles.viagemContainer}>
+              <Text style={styles.viagemTitle}>{viagem.nomeViagem}</Text>
+              <Text>Frete: R$ {viagem.valorFrete}</Text>
+              <Text>Distância: {viagem.distanciaViagem} Km</Text>
+              <Text>
+                Custo Combustível: R${' '}
+                {(
+                  parseFloat(viagem.preçoCombustivel) *
+                  (parseFloat(viagem.distanciaViagem) /
+                    parseFloat(mediaCaminhao || '1'))
+                ).toFixed(2)}
+              </Text>
+              <Text>Alimentação: R$ {viagem.alimentação}</Text>
+              <Text>Pagamento Ajudante: R$ {viagem.pagamentoAjudante}</Text>
+              <Text>Outros Custos: R$ {viagem.outrosCustos}</Text>
+              <Text>Lucro: R$ {viagem.lucro}</Text>
+              <View style={styles.aa}>
+                <Button
+                  title="Editar"
+                  onPress={() => handleEditViagem(index)}
+                />
+                <Button
+                  title="Excluir"
+                  onPress={() => handleRemoveViagem(index)}
+                />
+              </View>
+            </View>
+          ))
+        )}
+        <View style={styles.aa}>
+          <Button title="Excluir Todas as Viagens" onPress={handleRemoveAll} />
+          <Button title="Gerar PDF" onPress={handleGeneratePdf} />
         </View>
-    );
-};
+      </ScrollView>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-    aa: {
-        gap: 10,
-        margin: 10,
-    },
-    container: {
-        flex: 1,
-        padding: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    viagemContainer: {
-        marginBottom: 20,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-    },
-    viagemTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    message: {
-        textAlign: 'center',
-        fontSize: 18,
-        marginTop: 20,
-    },
-});
+  aa: {
+    gap: 10,
+    margin: 10,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  viagemContainer: {
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  viagemTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  message: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: 20,
+  },
+})
 
-export default Report;
+export default Report
